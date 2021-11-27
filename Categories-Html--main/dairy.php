205d108b4dcd4b42ea8e2ed15bd28fdfd182c536
Diem-Vu <71943800+Diem-Vu@users.noreply.github.com>
2021-11-26 19:49:17 -0800

    Add files via upload

---
+++ Categories-Html--main/dairy.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Grocery Store</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;250&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <div class="logo-container">
+        <img src="./img/logo (2).png" alt="">
+        <h4 class = "logo">Grocery Store</h4>
+      </div>
+      <nav>
+        <ul class="nav-links-left">
+          <li class="nav-link"><a href="index.php">Home</a></li>
+          <li class="nav-link"><a href="categories.php">Categories</a></li>
+          <li class="nav-link"><a href="#">About</a></li>
+          <li class="nav-link"><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="nav-links-right">
+        <ul>
+          <li class="nav-link-right"><a href="#">Login/Sign Up</a></li>
+          <li class="nav-link-right"><a href="#"><img src="./img/cart.svg" alt="cart"></a></li>
+        </ul>
+      </div>
+    </header>
+    <div class="banner">
+      <h1 class="banner-text"></h1>
+      <img src="img\banner(1).jpg" class="banner1">
+    </div>
+    <div class="main-text">
+      <h1>Organic Dairy</h1>
+    </div>
+    <div class="categories">
+    <div class="row">
+        <div class = "column">
+        <a href="#" class="category-item"><img src="Dairy/1%milk.jpg" ><h3>Organic Milk Low Fat 1% 1 Gallon </h3></a>
+        </div>
+        <div class="column">
+          <a href="#" class="category-item"><img src="Dairy/organicmilk2%.jpg"><h3>Organic Milk Reduced Fat 2% 1 Gallon </h3></a>
+        </div>
+        <div class="column">
+        <a href="#" class="category-item"><img src="Dairy/organicwholemilk.jpg" ><h3>Organic Milk Low Fat 1% 1 Gallon </h3></a>
+        </div>
+        </div>
+
+        <div class="row">
+            <div class = "column">
+            <a href="#" class="category-item"><img src="Dairy/organiceggslargebrown.jpg" ><h3>Organics Organic Eggs Large Brown - 12</h3></a>
+            </div>
+            <div class="column">
+              <a href="#" class="category-item"><img src="Dairy/organicgradeA.jpg" ><h3>Organics Eggs Grade A Omega 3 Large - 12 Count </h3></a>
+            </div>
+            <div class="column">
+            <a href="#" class="category-item"><img src="Dairy/1%Horizon.jpg"><h3> Horizon Organic Milk Vitamin D Half Gallon </h3></a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class = "column">
+            <a href="#" class="category-item"><img src="Dairy/horizon2%.jpg" ><h3>Horizon Organic 2% Reduced Fat Milk Half Gallon</h3></a>
+            </div>
+            <div class="column">
+              <a href="#" class="category-item"><img src="Dairy/cheeseshredded.jpg"><h3>Organic Cheese Finely Shredded Mexican Blend </h3></a>
+            </div>
+            <div class="column">
+            <a href="#" class="category-item"><img src="Dairy/cheeseslide.jpg"><h3>Organic Cheese Sliced White Cheddar</h3></a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class = "column">
+          <a href="#" class="category-item"><img src="Dairy/cheeseCottage.jpg"><h3>Organic Cheese Cottage 2% Milkfat Lowfat </h3></a>
+            </div>
+          </div>
+
+
+    </div>
+  </body>
+</html>
---
+++ Categories-Html--main/snackCategories.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Grocery Store</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <div class="logo-container">
+        <img src="./img/logo (2).png" alt="">
+        <h4 class = "logo">Grocery Store</h4>
+      </div>
+      <nav>
+        <ul class="nav-links-left">
+          <li class="nav-link"><a href="index.php">Home</a></li>
+          <li class="nav-link"><a href="categories.php">Categories</a></li>
+          <li class="nav-link"><a href="#">About</a></li>
+          <li class="nav-link"><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <div class="nav-links-right">
+        <ul>
+          <li class="nav-link-right"><a href="#">Login/Sign Up</a></li>
+          <li class="nav-link-right"><a href="#"><img src="./img/cart.svg" alt="cart"></a></li>
+        </ul>
+      </div>
+    </header>
+    <div class="banner">
+      <h1 class="banner-text"></h1>
+      <img src="img\banner(1).jpg" class="banner1">
+    </div>
+    <div class="main-text">
+      <h1>Organic Snacks</h1>
+    </div>
+    <div class="categories">
+    <div class="row">
+        <div class = "column">
+        <a href="#" class="category-item"><img src="Snacks/Annies-Homegrown-Organic-Classics-Crackers-Saltine-013562111206.png"><h3>Annies-Homegrown Organics Classisc</h3></a>
+        </div>
+        <div class="column">
+          <a href="#" class="category-item"><img src="Snacks/Artisan-Tropic-Cassava-Strips-Sea-Salt-868706000133.jpg"><h3>Tropic Cassava STrips Sea Salt</h3></a>
+        </div>
+        <div class="column">
+        <a href="#" class="category-item"><img src="Snacks/Back-To-Nature-Rice-Thin-Crackers-Multi-Seed-819898010011.jpg "><h3>Back To Nature Rice Thin Crackers Multi-Seed</h3></a>
+        </div>
+        </div>
+
+        <div class="row">
+            <div class = "column">
+            <a href="#" class="category-item"><img src="Snacks/Artisan-Tropic-Plantain-Strips-Gluten-Free-Paleo-Sea-Salt-868706000102.png"><h3>Artisan Tropic Cassava Strips Sea Salt </h3></a>
+            </div>
+            <div class="column">
+              <a href="#" class="category-item"><img src="Snacks/Jovial-Organic-Einkorn-Cookies-Vanilla-And-Chocolate-815421012118.png"><h3>Jovial Organic Einkorn Cookies Vanilla & Chocolate</h3></a>
+            </div>
+            <div class="column">
+            <a href="#" class="category-item"><img src="Snacks/Vitacost-Certified-Organic-Microwave-Popcorn-Non-GMO-Butter-844197025572.jpg"><h3>Vitacost Certified Organic Microwave Popcorn Non-GMO Butter</h3></a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class = "column">
+            <a href="#" class="category-item"><img src="Snacks/Kinnikinnick-Gluten-Free-Vanilla-Wafers-620133003251.jpg"><h3>Kinnikinnick Gluten Free Vanilla Wafers</h3></a>
+            </div>
+            <div class="column">
+              <a href="#" class="category-item"><img src="Snacks/Mediterranean-Organic-Red-Pepper-Stuffed-Green-Olives-814985000227.png"><h3>Mediterranean Organic Red Pepper Stuffed Green Olives </h3></a>
+            </div>
+            <div class="column">
+            <a href="#" class="category-item"><img src="Snacks/Siete-Grain-Free-Tortilla-Chips-Gluten-Free-Sea-Salt-851769007003.jpg"><h3>Siete Grain Free Tortilla Chips Gluten Free Sea Salt </h3></a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class = "column">
+          <a href="#" class="category-item"><img src="Snacks/SkinnyPop-Popcorn-Gluten-Free-White-Cheddar-850251004179.jpg"><h3>SkinnyPop Popcorn Gluten Free White</h3></a>
+            </div>
+          </div>
+
+
+    </div>
+  </body>
+</html>
